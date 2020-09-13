800c21d8c814dffeec3314695ce4371c6b5f3255
Mike Nawrocki <michal.m.nawrocki@gmail.com>
2020-09-13 21:29:01 +0100

    Feature - Include new 8bit font

--- space_invaders/entities/assets_loader.py
+++ space_invaders/entities/assets_loader.py
@@ -2,7 +2,9 @@ from pathlib import Path
 
 import pygame
 
+_font_path = Path().cwd().joinpath("assets/fonts")
 _root_path = Path().cwd().joinpath("assets/sprites/")
+
 SPACESHIP = pygame.image.load(str(_root_path.joinpath("player/ship.png").resolve()))
 ROCKET = pygame.image.load(str(_root_path.joinpath("player/rocket.png").resolve()))
 BUNKER_BLOCK = pygame.image.load(str(_root_path.joinpath("bunker/bunker_block.png").resolve()))
@@ -13,3 +15,6 @@ ALIEN_C = pygame.image.load(str(_root_path.joinpath("aliens/alien_c.png").resolv
 ALIEN_D = pygame.image.load(str(_root_path.joinpath("aliens/alien_d.png").resolve()))
 ALIEN_E = pygame.image.load(str(_root_path.joinpath("aliens/alien_e.png").resolve()))
 ALIEN_F = pygame.image.load(str(_root_path.joinpath("aliens/alien_f.png").resolve()))
+
+pygame.font.init()
+FONT_8BIT = pygame.font.Font(str(_font_path.joinpath("8_bit.ttf")), 27)
